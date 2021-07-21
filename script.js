b33be570288786c229c63fe2756b7d6e930c5e66
renanNun <renannunes400@gmail.com>
2021-07-21 14:48:38 -0300

    Adicionando mascara data

--- script.js
+++ script.js
@@ -58,29 +58,6 @@ function changeClass()
         body.classList.remove("new-body");
         body.classList.add("body-class"); 
     }
-
-
-
-  
-}
-
-function changeFormat()
-{   
-    let element = document.getElementById("inputMoney");
-    let valor = element.value;
-
-    valor = valor + '';
-    valor = parseInt(valor.replace(/[\D]+/g, ''));
-    valor = valor + '';
-    valor = valor.replace(/([0-9]{2})$/g, ",$1");
-
-    if (valor.length > 6) {
-        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    }
-
-    element.value = valor;
-    if(valor == 'NaN') element.value = '';
-    
 }
 
 function mascaraData(val) {
