14b013f1b4ae26fa8e19a145c1b8b66747ae914f
renanNun <renannunes400@gmail.com>
2021-07-21 14:44:25 -0300

    Corrigindo layout

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
 function addItem()
 {
-    var content = document.getElementById("inputItemList").value;
+    var content = document.getElementById("inputItem").children[0].value;
     //console.log(content);
-    if( content !== "")
+
+    if(content !== "")
     {
-        var element = document.createElement("li");
+        let element = document.createElement("li");
         element.classList.add("list-group-item");
         element.innerText = content;
-        document.getElementById("list").appendChild(element);
-        document.getElementById("inputItemList").value = "";
+        document.getElementById("listItem").children[0].appendChild(element);
+        document.getElementById("inputItem").children[0].value = "";
     }
 }
 
 function changeTitle()
 {
-    var title = document.getElementById("inputTitle").value;
+    var title = document.getElementById("image").children[2].value;
 
-    document.getElementById("title").innerHTML = title;
+    document.getElementById("image").children[0].innerHTML = title;
+
 }
 
 function setTitle()
 {
-    var title = document.getElementById("inputTitle").value;
-
+    var title = document.getElementById("image").children[2].value;
+    //console.log(title);
     if(title !== "") {
-       document.getElementById("title").innerHTML = title;
-       document.getElementById("inputTitle").value = "";
+       document.getElementById("image").children[0].innerHTML = title;
+       var title = document.getElementById("image").children[2].value = "";
     }
 }
 
 function changeUrlImg()
 {
-    if(document.getElementById("image").src === "https://media-exp1.licdn.com/dms/image/C4E0BAQHJVUdjXcahnQ/company-logo_200_200/0?e=2159024400&v=beta&t=jzN30LRzuT9F1uzXKHmVyd_KzeZOvH2tfhj22z2AWfQ")
+    var image = document.getElementById("image").children[1];
+    //console.log(image.src);
+
+    if(image.src === "https://media-exp1.licdn.com/dms/image/C4E0BAQHJVUdjXcahnQ/company-logo_200_200/0?e=2159024400&v=beta&t=jzN30LRzuT9F1uzXKHmVyd_KzeZOvH2tfhj22z2AWfQ")
     {
-        document.getElementById("image").src = "https://miro.medium.com/max/852/1*sSgZ71s6I_m4f37-7N7eYw.jpeg";
-        document.getElementById("image").style.height = "200px"
+        image.src = "https://miro.medium.com/max/852/1*sSgZ71s6I_m4f37-7N7eYw.jpeg";
+        image.style.height = "200px"
     }else{
-        document.getElementById("image").src = "https://media-exp1.licdn.com/dms/image/C4E0BAQHJVUdjXcahnQ/company-logo_200_200/0?e=2159024400&v=beta&t=jzN30LRzuT9F1uzXKHmVyd_KzeZOvH2tfhj22z2AWfQ";
-        document.getElementById("image").style.height = "200px"
+        image.src = "https://media-exp1.licdn.com/dms/image/C4E0BAQHJVUdjXcahnQ/company-logo_200_200/0?e=2159024400&v=beta&t=jzN30LRzuT9F1uzXKHmVyd_KzeZOvH2tfhj22z2AWfQ";
+        image.style.height = "200px"
     }
 
 }
@@ -77,3 +82,74 @@ function changeFormat()
     if(valor == 'NaN') element.value = '';
     
 }
+
+function mascaraData(val) {
+    var pass = val.value;
+    var expr = /[0123456789]/;
+  
+    for (i = 0; i < pass.length; i++) {
+      // charAt -> retorna o caractere posicionado no índice especificado
+      var lchar = val.value.charAt(i);
+      var nchar = val.value.charAt(i + 1);
+  
+      if (i == 0) {
+        // search -> retorna um valor inteiro, indicando a posição do inicio da primeira
+        // ocorrência de expReg dentro de instStr. Se nenhuma ocorrencia for encontrada o método retornara -1
+        // instStr.search(expReg);
+        if ((lchar.search(expr) != 0) || (lchar > 3)) {
+          val.value = "";
+        }
+  
+      } else if (i == 1) {
+  
+        if (lchar.search(expr) != 0) {
+          // substring(indice1,indice2)
+          // indice1, indice2 -> será usado para delimitar a string
+          var tst1 = val.value.substring(0, (i));
+          val.value = tst1;
+          continue;
+        }
+  
+        if ((nchar != '/') && (nchar != '')) {
+          var tst1 = val.value.substring(0, (i) + 1);
+  
+          if (nchar.search(expr) != 0)
+            var tst2 = val.value.substring(i + 2, pass.length);
+          else
+            var tst2 = val.value.substring(i + 1, pass.length);
+  
+          val.value = tst1 + '/' + tst2;
+        }
+  
+      } else if (i == 4) {
+  
+        if (lchar.search(expr) != 0) {
+          var tst1 = val.value.substring(0, (i));
+          val.value = tst1;
+          continue;
+        }
+  
+        if ((nchar != '/') && (nchar != '')) {
+          var tst1 = val.value.substring(0, (i) + 1);
+  
+          if (nchar.search(expr) != 0)
+            var tst2 = val.value.substring(i + 2, pass.length);
+          else
+            var tst2 = val.value.substring(i + 1, pass.length);
+  
+          val.value = tst1 + '/' + tst2;
+        }
+      }
+  
+      if (i >= 6) {
+        if (lchar.search(expr) != 0) {
+          var tst1 = val.value.substring(0, (i));
+          val.value = tst1;
+        }
+      }
+    }
+  
+    if (pass.length > 10)
+      val.value = val.value.substring(0, 10);
+    return true;
+  }
